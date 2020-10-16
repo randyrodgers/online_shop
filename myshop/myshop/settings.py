02b601f4cfeae501c076655f771338496cf7a1fc
randyrodgers <randy.e.rodgers@gmail.com>
2020-10-16 12:18:58 -0500

    added shopping cart settings and cart app

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +128,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join( BASE_DIR, 'media/' )
+
+CART_SESSION_ID = 'cart'

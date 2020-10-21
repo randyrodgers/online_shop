589f0303e4551cc24bcc472979573c9eb167cf22
randyrodgers <randy.e.rodgers@gmail.com>
2020-10-21 17:01:28 -0500

    bug fix for filename when exporting to csv

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -9,7 +9,7 @@ class OrderItemInline( admin.TabularInline ):
 
 def export_to_csv( modeladmin, request, queryset ):
     opts = modeladmin.model._meta
-    content_disposition = 'attachment; filename={opts.verbose_name}.csv'
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
     response = HttpResponse( content_type = 'text/csv' )
     response[ 'Content-Disposition' ] = content_disposition
     writer = csv.writer( response )

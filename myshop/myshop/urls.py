faa7c59ec10fd12b1117a4b8a266ba3fd0be80a1
randyrodgers <randy.e.rodgers@gmail.com>
2020-10-21 12:13:37 -0500

    added payment url path to project urls.py

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -4,9 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path( 'admin/', admin.site.urls),
     path( 'cart/', include( 'cart.urls', namespace = 'cart' ) ),
     path( 'orders/', include( 'orders.urls', namespace = 'orders' ) ),
+    path( 'payment/', include( 'payment.urls', namespace = 'payment' ) ),
     path( '', include( 'shop.urls', namespace = 'shop' ) ),
 ]
 

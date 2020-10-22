7659e0f1ba305fe57b251b4f678a40806e1ec805
randyrodgers <randy.e.rodgers@gmail.com>
2020-10-22 17:24:29 -0500

    added order_detail to orders admin.py

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from .models import Order, OrderItem
 import csv, datetime
 
@@ -30,6 +32,10 @@ def export_to_csv( modeladmin, request, queryset ):
     return response
 
 export_to_csv.short_description = 'Export to CSV'
+
+def order_detail( obj ):
+    url = reverse( 'orders:admin_order_detail', args = [obj.id] )
+    return mark_safe( f'<a href="{url}">View</a>' )
 
 @admin.register( Order )
 class OrderAdmin( admin.ModelAdmin ):

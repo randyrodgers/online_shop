319b6db30f94e404d97d943aec13d4af7e7bc891
randyrodgers <randy.e.rodgers@gmail.com>
2020-10-23 13:58:03 -0500

    added order_pdf to orderAdmin list_display

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -47,7 +47,7 @@ order_pdf.short_description = 'Invoice'
 class OrderAdmin( admin.ModelAdmin ):
     list_display = [ 'id', 'first_name', 'last_name', 'email',
                      'address', 'postal_code', 'city', 'paid',
-                     'created', 'updated', order_detail ]
+                     'created', 'updated', order_detail, order_pdf ]
     list_filter = [ 'paid', 'created', 'updated' ]
     inlines = [ OrderItemInline ]
     actions = [ export_to_csv ]
